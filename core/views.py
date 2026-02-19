29c272984c0589398bcc8429ba7dd5d6d22e3e15
Shriram Vyavahare <shriramprabhkarvyavahare07@gmail.com>
2026-02-19 18:58:57 +0530

    Disable database usage for Vercel serverless

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from .forms import AssignmentForm
-from .models import Assignment
 
 def home(request):
     username = request.COOKIES.get('username')
@@ -8,14 +7,17 @@ def home(request):
     if request.method == 'POST':
         form = AssignmentForm(request.POST, request.FILES)
         if form.is_valid():
-            assignment = form.save()
+            # ❌ Do NOT save to database on Vercel
+            student_name = form.cleaned_data.get('student_name')
+
             response = redirect('/')
-            response.set_cookie('username', assignment.student_name)
+            response.set_cookie('username', student_name)
             return response
     else:
         form = AssignmentForm()
 
-    assignments = Assignment.objects.all().order_by('-submitted_at')
+    # ❌ Disable database usage
+    assignments = []
 
     return render(request, 'home.html', {
         'form': form,
